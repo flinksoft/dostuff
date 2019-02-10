737f924bffff5def1a0db98aa768232ccfa86d16
Jordan <flinksoft@protonmail.ch>
2019-02-10 16:49:04 +0000

    initial commit

---
+++ Providers/IWeatherProvider.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using dostuff.Models;
+
+namespace dostuff.Providers
+{
+    public interface IWeatherProvider
+    {
+        List<WeatherForecast> GetForecasts();
+    }
+}
